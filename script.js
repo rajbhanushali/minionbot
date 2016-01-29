a126ddd7317e3ae218785e3c2c9845d0ae205428
Raj Bhanushali <raj.bshali@gmail.com>
2016-01-28 23:21:58 -0800

    made him kick ass

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 var login = require('facebook-chat-api');
+var readyforkick = false;
 
 login({email: "rjcombatarms@gmail.com", password: "tomato123"}, function callback (err, api) {
     if(err) return console.error(err);
 
-    /*api.listen(function callback(err, message) {
-    	console.log(message);
-        api.sendMessage(message.body, message.threadID);
-    });*/
 		api.setOptions({listenEvents: true});
 
 		    var stopListening = api.listen(function(err, event) {
@@ -18,13 +15,33 @@ login({email: "rjcombatarms@gmail.com", password: "tomato123"}, function callbac
 		              api.sendMessage("Goodbye...", event.threadID);
 		              return stopListening();
 		            }
+		            
 		            api.markAsRead(event.threadID, function(err) {
 		              if(err) console.log(err);
 		            });
 		            //api.sendMessage("TEST BOT: " + event.body, event.threadID);
-		            if(typeof event.attachments.stickerID !== 'undefined'){
-		       			api.sendMessage("TEST BOT: " + event.attachments.stickerID, event.threadID);
-		            }
+		           // console.log(event.attachments[1].stickerID);
+		           if(event.attachments.length !== 0 && event.attachments[0].type=="sticker"){
+		           		//api.sendMessage("stickerid: " + event.attachments[0].stickerID + " packid: " + event.attachments[0].packID, event.threadID);
+		           		if(event.attachments[0].packID == "1598049900458312" || event.attachments[0].packID == "210412585774633"){
+
+		           			api.sendMessage("get dat MOFO", event.threadID);
+
+		           			if(readyforkick){
+
+		           				api.sendMessage("kicking ur ass...", event.threadID);
+		           				api.removeUserFromGroup(event.senderID, event.threadID, function(err){if(err){api.sendMessage("error kicking", event.threadID);}else{api.sendMessage("u r ded", event.threadID);}});
+
+		           			}
+
+		           			readyforkick = true;
+		           		}
+		           }
+
+		           if(event.attachments.length == 0){
+		           	readyforkick=false;
+		           }
+		            
 		            break;
 		          case "event":
 		            console.log(event);
